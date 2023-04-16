3df3b03310831e0d96b76c2e2434e9691f39a09e
Luke Zhang <luke.zhang@latch.com>
2023-04-16 16:13:18 -0400

    Added app-ads.txt

--- tip-snap-info-react/src/App.tsx
+++ tip-snap-info-react/src/App.tsx
@@ -7,9 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <a href="/app-ads.txt" target="_blank" rel="noopener noreferrer"/>
         <a
           className="App-link"
           href="https://reactjs.org"
